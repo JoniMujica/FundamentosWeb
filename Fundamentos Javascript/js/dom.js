40b6c98979f908d8e1c807bc8105664df95e0314
JoniMujica <jonimujica3@hotmail.com>
2025-02-26 10:49:53 -0300

    intro DOM

---
+++ Fundamentos Javascript/js/dom.js
@@ -0,0 +1,23 @@
+//--------------------------------------------------------------------- */
+console.log("***************Elementos del documento****************");
+console.log(window.document);
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);//html
+console.log(document.doctype);
+console.log(document.charset);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+setTimeout(() => {
+    console.log(document.getSelection().toString());
+}, 3000);
+
+document.write("<h2>Hola Mundoooooo desde el DOM</h2>")
+
+
+
