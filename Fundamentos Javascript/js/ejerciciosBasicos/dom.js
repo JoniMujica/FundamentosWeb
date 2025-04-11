86fd94105540fe73acc7dc3eaba42d9384bc8313
JoniMujica <jonimujica3@hotmail.com>
2025-04-10 21:37:04 -0300

    91. DOM: Ejercicios Prácticos | Responsive Tester

--- Fundamentos Javascript/js/ejerciciosBasicos/dom.js
+++ Fundamentos Javascript/js/ejerciciosBasicos/dom.js
@@ -16,6 +16,7 @@ import countdown from "./cuenta_regresiva.js";
 import scrollTopButtom from "./boton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
+import responsiveTester from "./prueba_responsivve.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded",e=>{
@@ -26,6 +27,7 @@ d.addEventListener("DOMContentLoaded",e=>{
     scrollTopButtom(".scroll-top-btn");
     responsiveMedia("youtube","(min-width: 1024px)",`<a href="https://youtu.be/2SetvwBV-SU?list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA" target="_blank" rel="noopener">Ver Video</a>`,`<iframe width="560" height="315" src="https://www.youtube.com/embed/2SetvwBV-SU?si=1wtkHdme5SksF6Ex" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`);
     responsiveMedia("gmaps","(min-width: 1024px)",`<a href="https://maps.app.goo.gl/SuNgS1twbyj1q5Gv6" target="_blank" rel="noopener">Ver Mapa</a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.016888190834!2d-58.384150688871344!3d-34.60373445738601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses!2sar!4v1744312365714!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
+    responsiveTester("responsive-tester");
 })
 
 d.addEventListener("keydown",e=>{
