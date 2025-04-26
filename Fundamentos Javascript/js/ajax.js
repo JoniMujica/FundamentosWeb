245977012989b049c3b1aa06ddf3e949bc23e2c4
JoniMujica <jonimujica3@hotmail.com>
2025-04-26 13:45:25 -0300

    108. AJAX: API Fetch + Async-Await

--- Fundamentos Javascript/js/ajax.js
+++ Fundamentos Javascript/js/ajax.js
@@ -6,11 +6,11 @@
     
     xhr.addEventListener("readystatechange",e=>{
         if (xhr.readyState !== 4) return;
-        console.log(xhr);
+        //console.log(xhr);
         
         if(xhr.status>=200 && xhr.status < 300){
-            console.log("exito");
-            console.log(xhr.responseText);
+            //console.log("exito");
+            //console.log(xhr.responseText);
             //$xhr.innerHTML = xhr.responseText;
             let json = JSON.parse(xhr.responseText);
             console.log(json);
@@ -23,7 +23,7 @@
             $xhr.appendChild($fragment);
             
         }else{
-            console.log("error");
+            //console.log("error");
             let message = xhr.statusText || "Ocurrio un error";
             $xhr.innerHTML = `Error ${xhr.status}: ${message}`
         }
@@ -49,7 +49,7 @@
     //})
     .then((res)=>res.ok?res.json():Promise.reject(res))
     .then(json =>{
-        console.log(json);
+        //console.log(json);
         //$fetch.innerHTML = json;
         json.forEach(el => {
             const $li = document.createElement("li");
@@ -59,8 +59,42 @@
         $fetch.appendChild($fragment);
     })
     .catch(err=>{
-        console.log(err);
+        //console.log(err);
         let message = xhr.statusText || "Ocurrio un error";
         $fetch.innerHTML = `Error ${err.status}: ${message}`
     }).finally(()=> console.log("Esto se ejecutara independientemente del resultado de la promesa fetch"))
-})()
+})();
+
+(()=>{
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData(){
+        try {
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+            json = await res.json(); 
+            console.log(res,json);
+
+            //if (!res.ok) throw new Error("Ocurrio un error al solicitar los Datos");
+            if (!res.ok) throw {status: res.status, statusText: res.statusText};
+
+            json.forEach(el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li)
+            })
+            $fetchAsync.appendChild($fragment);
+            
+            
+        } catch (error) {
+            //console.log("Estoy en el catch", error);
+            let message = error.statusText || "Ocurrio un error";
+            $fetchAsync.innerHTML = `Error ${error.status}: ${message}`
+        }finally{
+            console.log("Esto se ejecutara independientemente del try... catch");
+        }
+    }
+    getData();
+
+
+})();
