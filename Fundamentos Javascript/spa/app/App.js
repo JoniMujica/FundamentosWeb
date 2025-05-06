45e24b578186f97df304a811033b9c3f23ec031d
JoniMujica <jonimujica3@hotmail.com>
2025-05-06 16:48:23 -0300

    147. SPA: Helper de peticiones AJAX

--- Fundamentos Javascript/spa/app/App.js
+++ Fundamentos Javascript/spa/app/App.js
@@ -1,6 +1,14 @@
 import api from "./helpers/wp_api.js";
+import { ajax } from "./helpers/ajax.js";
 
 export function App(){
+    ajax({
+        url: api.POSTS,
+        cbSuccess: (posts)=>{
+            console.log(posts);
+        }
+    })
+
     document.getElementById("root").innerHTML  = `<h1>Bienvenidos a mi primer SPA con vanilla SJ</h1>`
+    console.log(api);
 }
-console.log(api);
