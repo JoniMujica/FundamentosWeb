8ca586626e7a97a84d751704c67115171e1e05db
JoniMujica <jonimujica3@hotmail.com>
2025-05-06 23:09:02 -0300

    150. SPA: Renderizado Dinámico

--- Fundamentos Javascript/spa/app/helpers/wp_api.js
+++ Fundamentos Javascript/spa/app/helpers/wp_api.js
@@ -2,7 +2,7 @@ const NAME = "lucylara",
 DOMAIN = `https://${NAME}.com`,
 SITE = `${DOMAIN}/wp-json`,
 API_WP = `${SITE}/wp/v2`,
-POSTS = `${API_WP}/posts?_enbed`,
+POSTS = `${API_WP}/posts?_embed`,
 POST = `${API_WP}/posts`,
 SEARCH = `${API_WP}/search?_embed&search=`;
 
