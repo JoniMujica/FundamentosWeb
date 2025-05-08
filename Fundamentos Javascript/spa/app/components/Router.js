40d3380cfe1ec79472704c84cc0d74c1312a5665
JoniMujica <jonimujica3@hotmail.com>
2025-05-08 09:31:22 -0300

    151. SPA: Enrutamiento (Router)

---
+++ Fundamentos Javascript/spa/app/components/Router.js
@@ -0,0 +1,36 @@
+import { ajax } from "../helpers/ajax.js"
+import api from "../helpers/wp_api.js"
+import { PostCard } from "./PostCard.js";
+
+export function Router(){
+
+    const d = document,
+    w = window,
+    $posts = d.getElementById("posts");
+
+    let {hash} = location;
+    console.log(hash);
+
+    $posts.innerHTML = null;
+
+    if (!hash || hash === "#/") {
+        $posts.innerHTML = "<h2>Seccion del home</h2>";
+    }else if(hash.includes("#/search")){
+        $posts.innerHTML = "<h2>Seccion del buscador</h2>";
+    }else if (hash === "#/contacto") {
+        $posts.innerHTML = "<h2>Seccion del contacto</h2>";
+    }else{
+        $posts.innerHTML = "<h2>Aqui cargara el contenido  del post previamente seleccionado</h2>";
+    }
+    
+    //ajax({
+    //    url:api.POSTS,
+    //    cbSuccess: (posts)=>{
+    //        console.log(posts);
+    //        let html = "";
+    //        posts.forEach(post=> html += PostCard(post));
+    //        d.querySelector(".loader").style.display = "none";
+    //        d.getElementById("posts").innerHTML = html;
+    //    }
+    //})
+}
