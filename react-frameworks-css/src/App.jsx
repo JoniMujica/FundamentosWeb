30428102b9aff2e27dad99c865ec1c45ca707015
JoniMujica <jonimujica3@hotmail.com>
2025-05-27 23:39:07 -0300

    Curso React: 26. React Bootstrap & Material UI

--- react-frameworks-css/src/App.jsx
+++ react-frameworks-css/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 //import Bootstrap from './components/Bootstrap'
-import Bulma from './components/Bulma'
+//import Bulma from './components/Bulma'
+import ReactBootstrap from './components/ReactBootstrap'
+import MaterialUI from './components/MaterialUI'
 
 function App() {
 
@@ -8,8 +10,9 @@ function App() {
     <>
       <h1>Frameworks CSS con react</h1>
       {/*<Bootstrap />*/}
-      <Bulma />
-
+      {/*<Bulma />*/}
+      {/*<ReactBootstrap />*/}
+      <MaterialUI />
     </>
   )
 }
