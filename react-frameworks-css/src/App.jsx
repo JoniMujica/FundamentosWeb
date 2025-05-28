70c33e93655f7aa34e4ae200444bd83a367f8484
JoniMujica <jonimujica3@hotmail.com>
2025-05-27 22:43:01 -0300

    Curso React: 25. Frameworks CSS con React

---
+++ react-frameworks-css/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+//import Bootstrap from './components/Bootstrap'
+import Bulma from './components/Bulma'
+
+function App() {
+
+  return (
+    <>
+      <h1>Frameworks CSS con react</h1>
+      {/*<Bootstrap />*/}
+      <Bulma />
+
+    </>
+  )
+}
+
+export default App
