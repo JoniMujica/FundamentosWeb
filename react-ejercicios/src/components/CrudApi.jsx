61eec7e988c57d6cf5cc6b409980fce082952120
JoniMujica <jonimujica3@hotmail.com>
2025-05-30 19:47:59 -0300

    Curso React: 34. CRUD API: Inicializar el estado con AJAX ( 3 / 5 )

--- react-ejercicios/src/components/CrudApi.jsx
+++ react-ejercicios/src/components/CrudApi.jsx
@@ -1,11 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import CrudForm from './CrudForm'
 import CrudTable from './CrudTable'
+import { helperHtpp } from '../helpers/helpHttp';
 
 const CrudApi = () => {
     const [db, setDb] = useState([]);
-    
     const [dataToEdit, setDataToEdit] = useState(null);
+    let api = helperHtpp(),
+    url="http://localhost:5000/santos";
+
+    useEffect(() => {
+      api.get(url).then(res=>{
+        //console.log(res)
+        if (!res.err) {
+          setDb(res);
+        }else{
+          setDb(null)
+        }
+    });
+    }, []);
+    
+
 
     const createData = (data) =>{
       //console.log(data);
@@ -27,7 +42,7 @@ const CrudApi = () => {
     }
   return (
     <div>
-        <h2>Crud App</h2>
+        <h2>Crud Api</h2>
         <article className="grid-1-2">
           <CrudForm createData={createData} updateData={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit}/>
           <CrudTable data={db} deleteData={deleteData} setDataToEdit={setDataToEdit}/>
