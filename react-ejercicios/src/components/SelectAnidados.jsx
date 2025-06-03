e168609bb1ff0435d5026b57c8994f7fe5f19f1a
JoniMujica <jonimujica3@hotmail.com>
2025-06-02 23:57:23 -0300

    version ARG:  Curso React: 44. Selects Anidados: Renderizado de datos y estilos CSS ( 3 / 3 )

--- react-ejercicios/src/components/SelectAnidados.jsx
+++ react-ejercicios/src/components/SelectAnidados.jsx
@@ -1,25 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import SelectList from './SelectList'
+import SelectList from "./SelectList";
 
 const SelectAnidados = () => {
   const [state, setState] = useState("");
   const [town, setTown] = useState("");
   const [suburb, setSuburb] = useState("");
+  const TOKEN = "d81a7ac7-976d-4e1e-b7d3-b1979d791b6c";
+
   return (
     <div>
       <h2>Select Anidados</h2>
       <h3>Mexico</h3>
-      <SelectList title="estado" url="" handleChange={(e)=>setState(e.target.value)}/>
-      {state && <SelectList title="municipios" url="" handleChange={(e)=>setTown(e.target.value)}/>}
-      {town && <SelectList title="colonias" url="" handleChange={(e)=>setSuburb(e.target.value)}/>}
+      <SelectList
+        title="Provincia"
+        url={`https://apis.datos.gob.ar/georef/api/provincias`}
+        handleChange={(e) => {
+          setState(e.target.value);
+          setTown("");
+          setSuburb("");
+        }}
+      />
+      {state && (
+        <SelectList
+          title="Municipio"
+          url={`https://apis.datos.gob.ar/georef/api/municipios?provincia=${state}&aplanar=true&campos=estandar&max=500`}
+          handleChange={(e) => {
+            setTown(e.target.value);
+            setSuburb("");
+          }}
+        />
+      )}
+      {town && (
+        <SelectList
+          title="Localidad"
+          url={`https://apis.datos.gob.ar/georef/api/localidades?provincia=${state}&municipio=${town}&orden=nombre&aplanar=true&campos=estandar&max=500&exacto=true
+`}
+          handleChange={(e) => setSuburb(e.target.value)}
+        />
+      )}
       <pre>
         <code>
           {state} - {town} - {suburb}
         </code>
       </pre>
     </div>
-  )
-}
+  );
+};
 
-export default SelectAnidados
+export default SelectAnidados;
--- react-ejercicios/src/components/SelectList.jsx
+++ react-ejercicios/src/components/SelectList.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import { useFetch } from "../hooks/useFetch";
+import Loader from "./Loader";
+import Message from "./Message";
 
-const SelectList = () => {
+const SelectList = ({ title, url, handleChange }) => {
+  const { data, error, laoding } = useFetch(url);
+  console.log(data, error, laoding);
+
+  if (!data) return null;
+
+  if (error) {
+    return (
+      <Message
+        msg={`Error ${error.status}:${error.statusText}`}
+        bgColor="#dc3545"
+      />
+    );
+  }
+
+  let id = `select-${title}`;
+  let label = title.charAt(0).toUpperCase() + title.slice(1);
+  let options = data.provincias || data.municipios || data.localidades;
+  console.log(options);
+
   return (
-    <div>
-        <select name="" id="">
-            <option value="">---</option>
-        </select>
-    </div>
-  )
-}
+    <>
+      <label htmlFor={id}>{label}</label>
+      {laoding && <Loader />}
+      <select name={id} id={id} onChange={handleChange} selectIndex={0}>
+        <option value="">Elije un {title}</option>
+        {data &&
+          options.map((el) => <option value={el.nombre}>{el.nombre}</option>)}
+      </select>
+    </>
+  );
+};
 
-export default SelectList
+export default SelectList;
