2ff29709a0f52174308fb3edc7b3de6fa71afd3c
JoniMujica <jonimujica3@hotmail.com>
2025-06-04 16:09:12 -0300

    Curso React: 48. Validación Formularios: Envío de datos AJAX y API FormSubmit ( 4 / 4 )

--- react-ejercicios/src/hooks/useForm.js
+++ react-ejercicios/src/hooks/useForm.js
@@ -1,31 +1,57 @@
 import { useState } from 'react';
+import { helperHttp } from "../helpers/helpHttp";
 
-export const useForm = (initialForm,validateForm) => {
-    const [form, setForm] = useState(initialForm);
-    const [errors, setErrors] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [response, setResponse] = useState(null);
+export const useForm = (initialForm, validateForm) => {
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(null);
 
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: value,
-      });
-    };
-    const handleBlur = (e) => {
-      handleChange(e);
-      setErrors(validateForm(form));
-    };
-    const handleSubmit = (e) =>{}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(form));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateForm(form));
 
-    return{
-        form,
-        errors,
-        loading,
-        response,
-        handleChange,
-        handleBlur,
-        handleSubmit
+    if (Object.keys(errors).length === 0) {
+      alert("Enviando Formulario");
+      setLoading(true);
+      helperHttp()
+        .post("https://formsubmit.co/ajax/jonimujica34@gmail.com ", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: form,
+        })
+        .then((res) => {
+          setLoading(false);
+          setResponse(true);
+          setForm(initialForm);
+          setTimeout(() => setResponse(false), 3000);
+        });
+    } else {
+      return;
     }
+  };
+
+  return {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  };
 };
