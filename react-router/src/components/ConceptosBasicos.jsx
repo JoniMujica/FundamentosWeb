1161177d1873303881716ceb402d610470204cf8
JoniMujica <jonimujica3@hotmail.com>
2025-06-09 20:30:16 -0300

    Curso React: 55. React Router: Ruta de Error 404

--- react-router/src/components/ConceptosBasicos.jsx
+++ react-router/src/components/ConceptosBasicos.jsx
@@ -2,42 +2,58 @@ import React from 'react'
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
-
+import Home from '../pages/Home';
+import Error404 from '../pages/Error404';
 const ConceptosBasicos = () => {
   return (
     <div>
       <h2>ConceptosBasicos</h2>
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <h3>Home</h3>
-                <p>Bienvenidos al tema de las Rutas en React</p>
-              </>
-            }
-          />
-          <Route path="/acerca" element={<Acerca />} />
-          <Route path="/contacto" element={<Contacto />} />
-          {/* <Route
-            path="/contacto"
-            children={
-              <>
-                <Acerca />
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Excepturi eveniet est autem aliquid delectus inventore esse
-                  blanditiis deleniti dolor rerum, suscipit reiciendis ipsa?
-                  Magni expedita, mollitia distinctio doloribus dolores vitae!
-                </p>
-              </>
-            }
-          /> */}
+            <Route path='/' element={<Home />}/>
+            <Route path='/acerca' element={<Acerca />}/>
+            <Route path='/contacto' element={<Contacto />}/>
+            <Route path='*' element={<Error404 />}/>
         </Routes>
       </Router>
     </div>
   );
 };
+// const ConceptosBasicos = () => {
+//   return (
+//     <div>
+//       <h2>ConceptosBasicos</h2>
+//       <Router>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <>
+//                 <h3>Home</h3>
+//                 <p>Bienvenidos al tema de las Rutas en React</p>
+//               </>
+//             }
+//           />
+//           <Route path="/acerca" element={<Acerca />} />
+//           <Route path="/contacto" element={<Contacto />} />
+//         <Route
+//             path="/contacto"
+//             children={
+//               <>
+//                 <Acerca />
+//                 <p>
+//                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+//                   Excepturi eveniet est autem aliquid delectus inventore esse
+//                   blanditiis deleniti dolor rerum, suscipit reiciendis ipsa?
+//                   Magni expedita, mollitia distinctio doloribus dolores vitae!
+//                 </p>
+//               </>
+//             }
+//           /> *
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// };
 
 export default ConceptosBasicos
--- react-router/src/pages/Acerca.jsx
+++ react-router/src/pages/Acerca.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const Acerca = () => {
   return (
     <div>
-        <h2>Acerca</h2>
+      <h2>Acerca</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        repudiandae assumenda suscipit sit quae ipsum. Animi odit non cum labore
+        fugiat quas, officia sunt delectus velit, illum accusamus deserunt
+        repudiandae.
+      </p>
     </div>
-  )
+  );
 }
 
 export default Acerca
