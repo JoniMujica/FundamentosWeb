fb8bbe8696981ccdf9fab35a98048447945ce5bf
JoniMujica <jonimujica3@hotmail.com>
2025-06-09 19:43:02 -0300

    Curso React: 54. React Router: Sintaxis para declarar rutas

--- react-router/src/components/ConceptosBasicos.jsx
+++ react-router/src/components/ConceptosBasicos.jsx
@@ -1,24 +1,43 @@
 import React from 'react'
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import Acerca from "../pages/Acerca";
+import Contacto from "../pages/Contacto";
 
 const ConceptosBasicos = () => {
   return (
     <div>
-        <h2>ConceptosBasicos</h2>
-        <Router>
-            <Routes>
-                <Route path='/' element={
-                    <>
-                        <h3>Home</h3>
-                        <p>Bienvenidos al tema de las Rutas en React</p>
-                    </>
-                    } />
-                <Route path='/acerca' element={<h3>Acerca</h3>}/>
-                <Route path='/contacto' element={<h3>contacto</h3>} />
-            </Routes>
-        </Router>
+      <h2>ConceptosBasicos</h2>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h3>Home</h3>
+                <p>Bienvenidos al tema de las Rutas en React</p>
+              </>
+            }
+          />
+          <Route path="/acerca" element={<Acerca />} />
+          <Route path="/contacto" element={<Contacto />} />
+          {/* <Route
+            path="/contacto"
+            children={
+              <>
+                <Acerca />
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Excepturi eveniet est autem aliquid delectus inventore esse
+                  blanditiis deleniti dolor rerum, suscipit reiciendis ipsa?
+                  Magni expedita, mollitia distinctio doloribus dolores vitae!
+                </p>
+              </>
+            }
+          /> */}
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
 export default ConceptosBasicos
